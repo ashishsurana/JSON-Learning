ab05069c064823282a0186431a11d83bd9947837
Ashish Surana <ashishsurana@ymail.com>
2016-02-15 18:59:50 +0530

    API 23 UrlConnection

--- app/src/main/java/com/example/ashish/json/MainActivity.java
+++ app/src/main/java/com/example/ashish/json/MainActivity.java
@@ -19,6 +19,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -34,53 +35,87 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
-        final EditText editText = (EditText) findViewById(R.id.editText);
         button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Log.d("WORKING ","YES");
-                new ReadPlacesFeedTask().execute("http://api.geonames.org/postalCodeSearchJSON?postalcode=311021&maxRows=10&username=demo");
+                Log.d("WORKING", "Button PRESSED");
+//                new ReadPlacesFeedTask().execute("/http://api.geonames.org/postalCodeSearchJSON?postalcode=311021&maxRows=10&username=demo");
+                new ReadPlacesFeedTask().execute("http://www.getpincode.info/api/pincode?q=delhi");
             }
         });
+
     }
-    public String readJSONFeed(String URL){
-        StringBuilder stringBuilder = new StringBuilder();
-        HttpClient httpClient= new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(URL);
-        try {
-            HttpResponse response = httpClient.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-            Log.d("WORKING","inside 200");
-            if (statusCode==200){
-                Log.d("WORKING","inside 200");
-                HttpEntity entity = response.getEntity();
-                InputStream inputStream  =  entity.getContent();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(inputStream));
-                String line ;
-                while ((line = reader.readLine())!=null){
-                    stringBuilder.append(line);
-                }
-                inputStream.close();
-            }
-            else {
-                Log.d("JSON","Failed to Download");
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        return stringBuilder.toString();
-    }
+//    public String readJSONFeed(String URL){
+//        StringBuilder stringBuilder = new StringBuilder();
+//        HttpClient httpClient= new DefaultHttpClient();
+//        HttpGet httpGet = new HttpGet(URL);
+//        try {
+//            HttpResponse response = httpClient.execute(httpGet);
+//            StatusLine statusLine = response.getStatusLine();
+//            int statusCode = statusLine.getStatusCode();
+////            Log.d("WORKING","inside 200");
+//            if (statusCode==200){
+////                Log.d("WORKING","inside 200");
+//                HttpEntity entity = response.getEntity();
+//                InputStream inputStream  =  entity.getContent();
+//                BufferedReader reader = new BufferedReader(
+//                        new InputStreamReader(inputStream));
+//                String line ;
+//                while ((line = reader.readLine())!=null){
+//                    stringBuilder.append(line);
+//                }
+//                inputStream.close();
+//            }
+//            else {
+//                Log.d("JSON","Failed to Download");
+//            }
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return stringBuilder.toString();
+//    }
 
 
 
     private  class ReadPlacesFeedTask extends AsyncTask<String, Void, String>{
+        HttpURLConnection connection ;
+
         @Override
         protected String doInBackground(String... params) {
-            String temp = readJSONFeed(params[0]);
-            return temp;
+            Log.d("WORKING","Inside background");
+            StringBuilder temp = new StringBuilder();
+            try{
+                URL url = new URL(params[0]);
+
+                connection = (HttpURLConnection) url.openConnection();
+                int status = connection.getResponseCode();
+                if (status==200){
+                    InputStream inputStream = new BufferedInputStream(connection.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    Log.d("WORKING ", "HOLY1");
+                    if (reader.equals("")) {
+                        Log.d("WORKING ", "HOLY");
+                    }
+                    String line ;
+
+                    while ((line = reader.readLine()) != null) {
+                        temp.append(line);
+                        Log.d("WORKING ","YES ");
+                    }
+                }
+                else{
+                    Log.d("Status "," " + status);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                Log.d("WORKING" , e.toString());
+            } finally {
+                connection.disconnect();
+            }
+                Log.d("JSONFEED",temp.toString());
+            return temp.toString();
         }
 
         @Override
@@ -88,13 +123,8 @@ public class MainActivity extends Activity {
             try{
                 Log.d("WORKING ","YES In postexe");
                 JSONObject jsonObject = new JSONObject(s);
-                JSONArray postalcodeitems = new JSONArray(jsonObject.getString("postalCodes"));
-                Log.d("RESULT",postalcodeitems.toString());
-
-                for (int i=0; i<postalcodeitems.length(); i++){
-                    JSONObject postalCodesItem = postalcodeitems.getJSONObject(i);
-                    Log.d("RESULT1",postalCodesItem.getString("placeName"));
-                }
+                Log.d("FINAL",jsonObject.get("pincode").toString());
+                
             }
             catch (Exception e){
                 e.printStackTrace();
